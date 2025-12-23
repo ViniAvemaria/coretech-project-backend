9bae397f9737ffd8ba189c4b5926b4b6dffeddda
ViniAvemaria <160148129+ViniAvemaria@users.noreply.github.com>
2025-12-23 20:24:22 -0300

    First commit

---
+++ src/main/java/com/vinicius/coretech/CoreTechApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.coretech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinicius/coretech/CoreTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.coretech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
